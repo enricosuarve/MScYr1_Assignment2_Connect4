a25f33b5433caf2f7f1f59fedb8d56ed91ba114e
enricosuarve <snpreece@hotmail.com>
2020-12-03 14:46:38 +0000

    Fixed incrementor on line 148

--- src/com/simonpreece/MyConnectFour.java
+++ src/com/simonpreece/MyConnectFour.java
@@ -145,7 +145,7 @@ public class MyConnectFour {
     private void placeCounter(char player, int position){
         boolean placed = false;
         if(player == 'r'){
-            for(int i=board.length-1; i>=0; i++){
+            for(int i=board.length-1; i>=0; i--){
                 if(!placed){
                     if(board[i][position-1] == 'y'){
                         // skip
