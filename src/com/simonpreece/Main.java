9b39fe0e74bd44b22078f67102b157d13e802fb7
Simon Preece <snpreece@hotmail.com>
2020-12-19 18:52:40 +0000

    added --help to command line parameters and altered to accept uppercase N

--- src/com/simonpreece/Main.java
+++ src/com/simonpreece/Main.java
@@ -1,26 +1,41 @@
 //todo implement a command line help function with parameter info
 package com.simonpreece;
 
-class Main {
-    UI ui = new UI();
+import java.util.Locale;
 
+class Main {
     protected static View view = new ConsoleView();
+    UI ui = new UI();
 
     public static void main(String[] args) {
         int inARow = 0;
         Main m = new Main();
+        String helpMsg = "";
 
         for (String argString : args) {
-           if (argString.startsWith("n=")) {
+            if (argString.toLowerCase().startsWith("-n=")) {
                 try {
-                    inARow = Integer.parseInt(argString.substring(2).trim());
+                    inARow = Integer.parseInt(argString.substring(3).trim());
                 } catch (Exception e) {
                     //do nothing - just ignore and prompt for a value in the game as normal
                 }
             }
+            if (argString.equals("-?") || argString.equals("-h") || argString.equals("-help") || argString.equals("--help")) {
+                helpMsg += " Usage: Connect4 [options]\n\n";
+                helpMsg += "  where options include:\n\n";
+                helpMsg += "    -n=<value(3-6)>    Play ConnectN and set the number of counters in a row\n";
+                helpMsg += "                  required to win between 3 & 6.\n\n";
+                helpMsg += "    -? -h -help --help\n";
+                helpMsg += "                  print this help message to the output stream.";
+            }
         }
-        if (inARow > 0) {
+
+        if (helpMsg.length() > 0) {
+            Main.view.Display(helpMsg);
+        }
+        else if (inARow > 0) {
             new ConnectN(inARow);
+            Main.view.Display("\nGoodbye....\n\n");
         }
         else {
             boolean playAgain = true;
@@ -37,7 +52,8 @@ class Main {
                         break;
                 }
             }
+            Main.view.Display("\nGoodbye....\n\n");
         }
-        Main.view.Display("\nGoodbye....\n\n");
+
     }
 }
